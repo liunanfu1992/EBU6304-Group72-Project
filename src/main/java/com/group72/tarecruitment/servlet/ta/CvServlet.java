06e2f3dbbc3521d0da42acfaf976ddae9ddaa415
Nanfu Liu <liunanfu1992@gmail.com>
2026-03-25 14:38:34 +0800

    <feat> add cv module entry page

---
+++ src/main/java/com/group72/tarecruitment/servlet/ta/CvServlet.java
@@ -0,0 +1,17 @@
+package com.group72.tarecruitment.servlet.ta;
+
+import com.group72.tarecruitment.util.ViewPaths;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ta/cv")
+public class CvServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher(ViewPaths.TA_CV).forward(request, response);
+    }
+}
